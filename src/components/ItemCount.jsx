aa5f9df31f1bb13de89b2a1180216109a541c350
fililucas <lucas1929@live.com.ar>
2022-07-21 19:58:16 -0300

    codigo mas legible

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,28 +1,30 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 
 const ItemCount = (props) => {
-    const [count,setCount]=useState(props.initial);
-    function sumar(){if(count<props.stock){
-        setCount(count+1)};
+    const [count, setCount] = useState(props.initial);
+    function sumar() {
+        if (count < props.stock) {
+            setCount(count + 1)
+        };
     }
-    function quitar(){
-     if (count>=2)  { setCount(count-1)};
+    function quitar() {
+        if (count >= 2) { setCount(count - 1) };
     }
-    function finalizar(){
+    function finalizar() {
         alert("se agregaron " + count + " productos")
     }
-    return ( <>
-    <h2>comprar</h2>
-    <div>
-        <button onClick={quitar}>-</button>
-        <span>{count}</span>
-        <button onClick={sumar}>+</button>
+    return (<>
+        <h2>comprar</h2>
         <div>
-            <button onClick={finalizar}>agregar al carrito</button>
+            <button onClick={quitar}>-</button>
+            <span>{count}</span>
+            <button onClick={sumar}>+</button>
+            <div>
+                <button onClick={finalizar}>agregar al carrito</button>
+            </div>
         </div>
-    </div>
-    </> );
+    </>);
 }
- 
+
 export default ItemCount;
--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,34 +1,35 @@
-import React, {useState,useEffect  } from "react";
+import React, { useState, useEffect } from "react";
 import Productos from "../data/productos";
 import ItemDetail from "./ItemDetail";
-let promiseItem = new Promise ((resolve,reject)=>{
-    setTimeout(
-        () => {
-    resolve(Productos)  ;
-    
+let promiseItem = new Promise((resolve, reject) => {
+  setTimeout(
+    () => {
+      resolve(Productos);
+
     },
     2000);
-  });
+});
 
 const ItemDetailContainer = (props) => {
-    
-      
-      let [item,setItem] = useState([]);
-      useEffect(()=>{   
-      promiseItem.then(
-        (respuesta)=>{
-          setItem(respuesta[0]);
-        }
-        ).catch ((errorMg)=>console.error((errorMg)))},
-      [])
 
-    return ( 
-        <>
-        <ItemDetail item={item}/>
-       
-      
-        </>
-     );
+
+  let [item, setItem] = useState([]);
+  useEffect(() => {
+    promiseItem.then(
+      (respuesta) => {
+        setItem(respuesta[0]);
+      }
+    ).catch((errorMg) => console.error((errorMg)))
+  },
+    [])
+
+  return (
+    <>
+      <ItemDetail item={item} />
+
+
+    </>
+  );
 }
- 
+
 export default ItemDetailContainer;
